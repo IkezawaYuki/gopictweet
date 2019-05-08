852cba696bf37d6d36e88ef4c9dc14eafd283ac1
IkezawaYuki <y.ikezawa93@gmail.com>
2019-05-08 22:37:04 +0900

    add main method

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import "net/http"
 
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", index)
+
+	server := &http.Server{
+		Addr:    "127.0.0.1:8080",
+		Handler: mux,
+	}
+	server.ListenAndServe()
 }

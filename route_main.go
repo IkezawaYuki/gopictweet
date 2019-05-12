9707c7f290c040830085ad8f30738b7b13a6e661
yikezawa <y.ikezawa93@gmail.com>
2019-05-12 22:10:57 +0900

    viewの追加

--- route_main.go
+++ route_main.go
@@ -11,6 +11,6 @@ func index(writer http.ResponseWriter, response *http.Request) {
 	tweets, err := data.Tweets()
 	if err != nil {
 		fmt.Println("not err!")
-		generateHTML(writer, tweets, "layout", "public.navbar", "index")
+		generateHTML(writer, tweets, "layout", "public.navbar", "index", "tweet")
 	}
 }

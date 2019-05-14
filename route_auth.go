aa36e33aba8942226743ee21d0f02377b5825b16
IkezawaYuki <y.ikezawa93@gmail.com>
2019-05-14 23:26:46 +0900

    signup関数の追加

--- route_auth.go
+++ route_auth.go
@@ -8,3 +8,7 @@ func login(writer http.ResponseWriter, request *http.Request) {
 	template := parseTemplateFiles("login.layout", "public.navbar", "login")
 	template.Execute(writer, nil)
 }
+
+func signup(writer http.ResponseWriter, request *http.Request) {
+	generateHTML(writer, nil, "login.layout", "public.navbar", "signup")
+}

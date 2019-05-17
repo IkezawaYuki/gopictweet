068748833e24688a1f8d069238752ebf331b6c8f
IkezawaYuki <y.ikezawa93@gmail.com>
2019-05-17 22:09:33 +0900

    読み込みと新規作成のハンドラを実装

--- route_tweet.go
+++ route_tweet.go
@@ -1,6 +1,10 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"gopictweet/data"
+	"net/http"
+)
 
 func newTweet(w http.ResponseWriter, r *http.Request) {
 	_, err := session(w, r)
@@ -8,5 +12,45 @@ func newTweet(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/login", 302)
 	} else {
 		generateHTML(w, r, "layout", "public.navbar", "new.tweet")
+	}
+}
+
+func createTweet(w http.ResponseWriter, r *http.Request) {
+	ses, err := session(w, r)
+	if err != nil {
+		http.Redirect(w, r, "/login", 302)
+	} else {
+		err = r.ParseForm()
+		if err != nil {
+			fmt.Println("cannnot parse")
+			panic(err)
+		}
+		user, err := ses.User()
+		if err != nil {
+			panic(err)
+		}
+		text := r.PostFormValue("text")
+		image := r.PostFormValue("image")
+		if _, err := user.CreateTweet(text, image); err != nil {
+			fmt.Println("cannot create tweet")
+			panic(err)
+		}
+		http.Redirect(w, r, "/", 302)
+	}
+}
+
+func readTweet(w http.ResponseWriter, r *http.Request) {
+	vals := r.URL.Query()
+	uuid := vals.Get("id")
+	tweet, err := data.TweetByUuid(uuid)
+	if err != nil {
+		fmt.Println("error is occured")
+	} else {
+		_, err = session(w, r)
+		if err != nil {
+			generateHTML(w, &tweet, "layout", "public.nuvbar", "public.tweet")
+		} else {
+			generateHTML(w, &tweet, "layout", "private.navbar", "private.tweet")
+		}
 	}
 }
